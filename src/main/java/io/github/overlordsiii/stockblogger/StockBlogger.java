6e54b0189347866e0d5b74dec15744b181d9e64e
OverlordsIII <shrihunreddy@outlook.com>
2024-02-12 22:34:30 -0800

    Add request to get current stock price

--- src/main/java/io/github/overlordsiii/stockblogger/StockBlogger.java
+++ src/main/java/io/github/overlordsiii/stockblogger/StockBlogger.java
@@ -4,6 +4,13 @@ import io.github.overlordsiii.stockblogger.config.PropertiesHandler;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
 import java.util.Scanner;
 
 public class StockBlogger {
@@ -12,7 +19,7 @@ public class StockBlogger {
 
     public static final PropertiesHandler API_KEY = PropertiesHandler
             .builder()
-            .addConfigOption("polygonIoApiKey", "")
+            .addConfigOption("stockAPIKey", "")
             .setFileName("api_keys.properties")
             .build();
 
@@ -22,5 +29,29 @@ public class StockBlogger {
         System.out.println("What stock do you want information on?: ");
 
         String stockName = scanner.nextLine();
+
+        try {
+            URL url = new URL("https://api.twelvedata.com/price?symbol=" + stockName + "&apikey=" + API_KEY.getConfigOption("stockAPIKey"));
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("Accept", "application/json");
+
+            if (conn.getResponseCode() != 200) {
+                throw new RuntimeException("Failed : HTTP error code : " + conn.getResponseCode());
+            }
+
+            BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
+
+            String output;
+            System.out.println("Output from Server .... \n");
+            while ((output = br.readLine()) != null) {
+                System.out.println(output);
+            }
+
+            conn.disconnect();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
